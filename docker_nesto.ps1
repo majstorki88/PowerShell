dac2ab443892eee204b5f44b6dd79776cce01819
Nemanja Majstorović <nemanja.majstorovic@asseco-see.rs>
2017-11-17 10:34:06 +0100

    Adding PS docker

---
+++ docker_nesto.ps1
@@ -0,0 +1,9 @@
+﻿
+docker-machine ls
+
+docker pull brunosarnaglia/vasco
+docker run -i --expose=22 b5593e60c33b bash
+
+
+
+
--- quick_docker.ps1
+++ quick_docker.ps1
@@ -32,3 +32,19 @@ docker push majstorki88/drone-node
 
 #docker prune
 docker system prune
+
+
+#build image from docker file
+docker build . -t asseco/docker-agent
+
+#expose port for container
+docker run -ti --rm jboss/keycloak -p 8080
+
+#run keycloack with username and pass
+docker run -it --expose=8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin jboss/keycloak
+
+#run MSSQL container
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=banatkikinda' -p 1433:1433 -d microsoft/mssql-server-linux:2017-latest
+
+#run MySQL container
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=banatkikinda -d mysql:latest
